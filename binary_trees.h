e9799c99a635eb9673195949cdd586ec5ad9a5b7
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-03-28 19:54:48 +0100

    done: Delete

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREE_H
-#define BINARY_TREE_H
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stdlib.h>
 #include <stddef.h>
@@ -39,5 +39,5 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-
-#endif /*BINARY_TREE_H*/
+void binary_tree_delete(binary_tree_t *tree);
+#endif /*BINARY_TREES_H*/
