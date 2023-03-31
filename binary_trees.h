3a9445bef97a3b9f44b4bf695010252fee9d4bfe
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-03-31 13:31:35 +0100

    done: BST - search

--- binary_trees.h
+++ binary_trees.h
@@ -61,4 +61,5 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *,
 int _pow_recursion(int, int);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
 #endif /*BINARY_TREES_H*/
